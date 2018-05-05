ab191aad5b94b280f50dd96b02739e5b46c22979
TGDBRAN6 <andre.brunner2@swisscom.com>
2018-05-05 13:12:25 +0200

    debug 22

--- src/logstash/supply/supply.go
+++ src/logstash/supply/supply.go
@@ -617,6 +617,7 @@ func (gs *Supplier) PipInstallCurator() error {
     			# --no-index prevents contacting pypi to download packages
     			# --find-links tells pip where to look for the dependancies
     			pip3 install --no-index --find-links %s/dependencies --install-option="--prefix=%s/curator" elasticsearch-curator -v
+				pip3 list
 				`,  filepath.Join(gs.Stager.DepDir()), filepath.Join(gs.Curator.StagingLocation), filepath.Join(gs.Stager.DepDir()) ))
 
 	if err := gs.WriteScript(scriptName, content); err != nil {
