12f998e8be3aec64ae2f31b74bd8f872762383ba
TGDBRAN6 <andre.brunner2@swisscom.com>
2018-05-19 16:54:25 +0200

    debug 15

--- src/logstash/supply/supply_helper.go
+++ src/logstash/supply/supply_helper.go
@@ -1,24 +1,23 @@
 package supply
 
 import (
-	"os"
-	"strings"
-	"github.com/andibrunner/libbuildpack"
-	"path/filepath"
-	"io/ioutil"
-	"fmt"
-	"logstash/util"
-	"io"
-	"os/exec"
 	"bufio"
+	"fmt"
+	"github.com/andibrunner/libbuildpack"
+	"io"
+	"io/ioutil"
+	"logstash/util"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
-
 
 func (gs *Supplier) BPDir() string {
 	return gs.BuildpackDir
 }
 
-func (gs *Supplier) NewDependency(name string, versionParts int, configVersion string, doCompile bool) (Dependency, error){
+func (gs *Supplier) NewDependency(name string, versionParts int, configVersion string, doCompile bool) (Dependency, error) {
 	var dependency = Dependency{Name: name, VersionParts: versionParts, ConfigVersion: configVersion}
 
 	if parsedVersion, err := gs.SelectDependencyVersion(dependency); err != nil {
@@ -27,7 +26,7 @@ func (gs *Supplier) NewDependency(name string, versionParts int, configVersion s
 	} else {
 		dependency.Version = parsedVersion
 		dependency.DoCompile = doCompile
-		dependency.FullName = dependency.Name+"-"+dependency.Version
+		dependency.FullName = dependency.Name + "-" + dependency.Version
 		dependency.RuntimeLocation = gs.EvalRuntimeLocation(dependency)
 		dependency.StagingLocation = gs.EvalStagingLocation(dependency)
 		dependency.CacheLocation = gs.EvalCacheLocation(dependency)
@@ -38,11 +37,10 @@ func (gs *Supplier) NewDependency(name string, versionParts int, configVersion s
 	return dependency, nil
 }
 
-
 func (gs *Supplier) WriteDependencyProfileD(dependencyName string, content string) error {
 
 	if err := gs.Stager.WriteProfileD(dependencyName+".sh", content); err != nil {
-		gs.Log.Error("Error writing profile.d script for %s: %s", dependencyName,err.Error())
+		gs.Log.Error("Error writing profile.d script for %s: %s", dependencyName, err.Error())
 		return err
 	}
 	return nil
@@ -53,7 +51,7 @@ func (gs *Supplier) ReadCachedDependencies() error {
 	//clean up Cache
 	if gs.LogstashConfig.Buildpack.NoCache {
 		util.RemoveAllContents(gs.Stager.CacheDir()) // rm -r cache_dir/*
-	}else {
+	} else {
 		//if dependency dir with current supplier version doesn't exist delete all content in cache
 		// cache_dir/dependencies/[supplier_version]/
 		parentDir := filepath.Dir(gs.DepCacheDir)
@@ -62,7 +60,7 @@ func (gs *Supplier) ReadCachedDependencies() error {
 		}
 	}
 	gs.CachedDeps = make(map[string]string)
-	os.MkdirAll(gs.DepCacheDir,0755)
+	os.MkdirAll(gs.DepCacheDir, 0755)
 
 	cacheDir, err := ioutil.ReadDir(gs.DepCacheDir)
 	if err != nil {
@@ -70,7 +68,7 @@ func (gs *Supplier) ReadCachedDependencies() error {
 		return err
 	}
 
-	for _, dirEntry := range cacheDir{
+	for _, dirEntry := range cacheDir {
 		gs.Log.Debug(fmt.Sprintf("--> added dependency '%s' to cache list", dirEntry.Name()))
 		gs.CachedDeps[dirEntry.Name()] = ""
 	}
@@ -78,13 +76,12 @@ func (gs *Supplier) ReadCachedDependencies() error {
 	return nil
 }
 
-
 func (gs *Supplier) InstallDependency(dependency Dependency) error {
 	var err error
 
 	//check if there are other cached versions of the same dependency
-	for cachedDep := range gs.CachedDeps{
-		if strings.HasPrefix(cachedDep, dependency.Name + "-") && cachedDep != dependency.FullName{
+	for cachedDep := range gs.CachedDeps {
+		if strings.HasPrefix(cachedDep, dependency.Name+"-") && cachedDep != dependency.FullName {
 			gs.Log.Debug(fmt.Sprintf("--> deleting unused dependency version '%s' from application cache", cachedDep))
 			gs.CachedDeps[cachedDep] = "deleted"
 			os.RemoveAll(filepath.Join(gs.DepCacheDir, cachedDep))
@@ -127,7 +124,7 @@ func (gs *Supplier) InstallDependency(dependency Dependency) error {
 
 		//extract tarball
 		extractLocation := dependency.CacheLocation
-		if dependency.DoCompile{
+		if dependency.DoCompile {
 			extractLocation = dependency.TmpExtractLocation
 		}
 
@@ -144,30 +141,30 @@ func (gs *Supplier) InstallDependency(dependency Dependency) error {
 			err = os.Rename(tarball, extractLocation)
 		}
 
-		if err != nil{
+		if err != nil {
 			gs.Log.Error("Error extracting '%s': %s", dependency.Name, err.Error())
 			return err
 		}
 
 		//compile dependency
-		if dependency.DoCompile{
+		if dependency.DoCompile {
 			gs.CompileDependency(dependency, filepath.Join(extractLocation, gs.Python3.Name), dependency.CacheLocation)
 		}
 
-	}else{ //cached
+	} else { //cached
 		gs.Log.BeginStep("Installing %s %s from application cache", manifestDependency.Name, manifestDependency.Version)
 	}
 
 	// copy from cache to Stage
 	err = gs.CopyToStage(dependency)
-	if err != nil{
+	if err != nil {
 		gs.Log.Error("Error copying '%s' to stage: %s", dependency.Name, err.Error())
 		return err
 	}
 
 	//remove cache if defined
 	if gs.LogstashConfig.Buildpack.NoCache {
-		os.RemoveAll(filepath.Join(gs.DepCacheDir,dependency.FullName))
+		os.RemoveAll(filepath.Join(gs.DepCacheDir, dependency.FullName))
 	}
 
 	//register dependency
@@ -176,22 +173,22 @@ func (gs *Supplier) InstallDependency(dependency Dependency) error {
 	return nil
 }
 
-func (gs *Supplier) CopyToStage(dep Dependency) error{
-	out , err := exec.Command("cp", "-r", dep.CacheLocation + "/.", dep.StagingLocation ).Output()
+func (gs *Supplier) CopyToStage(dep Dependency) error {
+	out, err := exec.Command("cp", "-r", dep.CacheLocation+"/.", dep.StagingLocation).Output()
 	if err != nil {
-		gs.Log.Error(string(out));
+		gs.Log.Error(string(out))
 		return err
 	}
 	return nil
 }
 
-func (gs *Supplier) LsDir(dir string) error{
-	out , err := exec.Command("ls", "-al", dir ).Output()
+func (gs *Supplier) LsDir(dir string) error {
+	out, err := exec.Command("ls", "-al", dir).Output()
 	if err != nil {
-		gs.Log.Error(string(out));
+		gs.Log.Error(string(out))
 		return err
 	}
-	gs.Log.Info(string(out));
+	gs.Log.Info(string(out))
 	return nil
 }
 
@@ -201,7 +198,7 @@ func (gs *Supplier) CompileDependency(dep Dependency, makeDir string, prefix str
 	gs.Log.BeginStep("Starting Compilation of %s (Compilation only needs to be done the first time)", dep.FullName)
 
 	gs.Log.Info("Step 1 of 3: configure ...")
-	cmd := exec.Command("/bin/sh", filepath.Join(makeDir, "configure"), fmt.Sprintf("--prefix=%s",prefix) )
+	cmd := exec.Command("/bin/sh", filepath.Join(makeDir, "configure"), fmt.Sprintf("--prefix=%s", prefix))
 	cmd.Dir = makeDir
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
@@ -218,7 +215,7 @@ func (gs *Supplier) CompileDependency(dep Dependency, makeDir string, prefix str
 	if strings.ToLower(gs.LogstashConfig.Buildpack.LogLevel) == "debug" {
 		go gs.copyOutput(stdout, "stdout")
 		go gs.copyOutput(stderr, "stderr")
-	}else{
+	} else {
 		go gs.copyOutput(stdout, "none")
 		go gs.copyOutput(stderr, "none")
 	}
@@ -249,7 +246,7 @@ func (gs *Supplier) CompileDependency(dep Dependency, makeDir string, prefix str
 	if strings.ToLower(gs.LogstashConfig.Buildpack.LogLevel) == "debug" {
 		go gs.copyOutput(stdout, "stdout")
 		go gs.copyOutput(stderr, "stderr")
-	}else{
+	} else {
 		go gs.copyOutput(stdout, "none")
 		go gs.copyOutput(stderr, "none")
 	}
@@ -280,7 +277,7 @@ func (gs *Supplier) CompileDependency(dep Dependency, makeDir string, prefix str
 	if strings.ToLower(gs.LogstashConfig.Buildpack.LogLevel) == "debug" {
 		go gs.copyOutput(stdout, "stdout")
 		go gs.copyOutput(stderr, "stderr")
-	}else{
+	} else {
 		go gs.copyOutput(stdout, "none")
 		go gs.copyOutput(stderr, "none")
 	}
@@ -295,9 +292,9 @@ func (gs *Supplier) CompileDependency(dep Dependency, makeDir string, prefix str
 
 }
 
-func (gs *Supplier) RemoveUnusedDependencies () error{
+func (gs *Supplier) RemoveUnusedDependencies() error {
 
-	for cachedDep, value := range gs.CachedDeps{
+	for cachedDep, value := range gs.CachedDeps {
 		if value == "" {
 			gs.Log.Debug(fmt.Sprintf("--> deleting unused dependency '%s' from application cache", cachedDep))
 			os.RemoveAll(filepath.Join(gs.DepCacheDir, cachedDep))
@@ -305,7 +302,6 @@ func (gs *Supplier) RemoveUnusedDependencies () error{
 	}
 	return nil
 }
-
 
 func (gs *Supplier) SelectDependencyVersion(dependency Dependency) (string, error) {
 
@@ -357,7 +353,6 @@ func (gs *Supplier) EvalTmpExtractLocation(dependency Dependency) string {
 	return filepath.Join(gs.DepTmpExtractDir, dependency.FullName)
 }
 
-
 func (gs *Supplier) WriteScript(scriptName, scriptContents string) error {
 	scriptsDir := filepath.Join(gs.Stager.DepDir(), "scripts")
 
@@ -372,7 +367,7 @@ func (gs *Supplier) WriteScript(scriptName, scriptContents string) error {
 func (gs *Supplier) ExecScript(scriptName string) error {
 	scriptsDir := filepath.Join(gs.Stager.DepDir(), "scripts")
 
-	cmd := exec.Command("/bin/sh", filepath.Join(scriptsDir, scriptName) )
+	cmd := exec.Command("/bin/sh", filepath.Join(scriptsDir, scriptName))
 
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
@@ -390,7 +385,7 @@ func (gs *Supplier) ExecScript(scriptName string) error {
 	if strings.ToLower(gs.LogstashConfig.Buildpack.LogLevel) == "debug" {
 		go gs.copyOutput(stdout, "stdout")
 		go gs.copyOutput(stderr, "stderr")
-	}else{
+	} else {
 		go gs.copyOutput(stdout, "none")
 		go gs.copyOutput(stderr, "none")
 	}
